6f5758dd058d1298a854484255e9c444f68f7149
Jon Childress <jon@childr.es>
2015-03-08 13:39:12 -0500

    CC functions pass specs

--- cis194/homework-1/src/HW1/CreditCard.hs
+++ cis194/homework-1/src/HW1/CreditCard.hs
@@ -6,19 +6,44 @@ module HW1.CreditCard
 , validate
 ) where
 
+import Data.List (foldl', unfoldr)
+import Data.Tuple (swap)
+
 type CCNumber = Integer
 
 toDigits :: CCNumber -> [Integer]
-toDigits = undefined
+toDigits = reverse . toDigitsRev
 
 toDigitsRev :: CCNumber -> [Integer]
-toDigitsRev = undefined
+toDigitsRev = unfoldr nextDigit
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = reverse . doubleOther . reverse
 
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits = foldl' (\total -> (+) total . sum . toDigitsRev) 0
 
 validate :: CCNumber -> Bool
-validate = undefined
+validate ccn = remain == 0
+  where
+    (_,remain) =  ccnHash ccn `divMod` 10
+
+-- private functions
+
+nextDigit :: Integer -> Maybe (Integer, Integer)
+nextDigit n =
+    if n > 0
+    then Just . swap $ n `divMod` 10
+    else Nothing
+
+doubleIfFlag :: (Bool, Integer) -> Integer
+doubleIfFlag (flag, n) =
+    if flag
+    then 2 * n
+    else n
+
+doubleOther :: [Integer] -> [Integer]
+doubleOther = zipWith (curry doubleIfFlag) (cycle [False, True])
+
+ccnHash :: CCNumber -> Integer
+ccnHash = sumDigits . doubleOther . toDigitsRev
