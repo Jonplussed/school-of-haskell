e382e6c89d3a4cb5e358a3112be02416bb009a6d
Jon Childress <jon@childr.es>
2015-03-23 12:55:40 -0500

    I think this Hanoi algo works

--- cis194/homework-1/src/HW1/TowersOfHanoi.hs
+++ cis194/homework-1/src/HW1/TowersOfHanoi.hs
@@ -4,4 +4,7 @@ type Peg = Char
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi discs p1 p2 p3
+  | discs < 1  = []
+  | discs == 1 = [(p1, p2)]
+  | otherwise  = [(p1, p3), (p1, p2), (p3, p2)] ++ hanoi (discs - 2) p1 p2 p3
