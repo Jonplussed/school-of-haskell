8e25cf55d4cec7be5735623e7f0a6af7d358afc1
Jon Childress <jon@childr.es>
2015-03-26 04:10:25 -0500

    first pass at CIS194 HW 3 solutions

--- cis194/homework-3/src/HW3/Golf.hs
+++ cis194/homework-3/src/HW3/Golf.hs
@@ -4,11 +4,47 @@ module HW3.Golf
 , histogram
 ) where
 
+import Data.List
+import Data.Char
+
 skips :: [a] -> [[a]]
-skips = undefined
+skips list = zipWith (\n _ -> every n list) [1..] list
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima (x:y:z:zs) =
+    if y > x && y > z
+    then y : localMaxima (z:zs)
+    else localMaxima (y:z:zs)
+localMaxima _ = []
 
 histogram :: [Integer] -> String
-histogram = undefined
+histogram = unlines . transpose . histColumns . map fromIntegral
+
+-- private
+
+every :: Integer -> [a] -> [a]
+every n list = every' n list
+  where
+    every' _ []     = []
+    every' 1 (x:xs) = x : every' n xs
+    every' r (_:xs) = every' (r - 1) xs
+
+histColumn :: Int -> (Int, Int) -> String
+histColumn rows (n, count) =
+    replicate (rows - count) ' ' ++
+    replicate count '*' ++
+    ['=', intToDigit  n]
+
+histColumns :: [Int] -> [String]
+histColumns ns = map column $ zip histRng counts
+  where
+    column = histColumn $ maximum counts
+    counts = histCounts ns
+
+histRng :: [Int]
+histRng = [0..9]
+
+histCounts :: [Int] -> [Int]
+histCounts = map count . group . sort . (histRng ++)
+  where
+    count ns = length ns - 1
